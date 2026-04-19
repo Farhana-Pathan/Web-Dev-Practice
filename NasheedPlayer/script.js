7a195bd02703621f466334f5550e99bbdd9fc395
Farhana-Pathan <farhanapathan174@gmail.com>
2026-04-19 21:59:47 +0530

    Developing a Nasheed Player

---
+++ NasheedPlayer/script.js
@@ -0,0 +1,20 @@
+const nasheed = [
+    {
+        image: "image/al-aqsa.jpg",
+        name: "Darbuna Darbun",
+        artist: "Yasser Farouk Abu Ammar",
+        audio: "audio/darbona-darb-taweel.mp3"
+    },
+    {
+        image: "image/madinah.jpg",
+        name: "Kon Anta",
+        artist: "Humood Alkhudher",
+        audio: "audio/Kon_Anta.mp3"
+    },
+    {
+        image: "image/makka.jpg",
+        name: "Yanabi Salam Alik",
+        artist: "Maher Zain",
+        audio: "audio/Yanabi_salam_alik.mp3"
+    }
+]
